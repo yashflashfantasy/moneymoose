88c9c03400b123be0692d1d906b44f245c2e2f19
Yash Chanda <yash.chanda@adit.com>
2025-12-15 07:17:43 +0530

    Added Fresh Tokens

--- assets/js/load-stocks.001.js
+++ assets/js/load-stocks.001.js
@@ -133,7 +133,7 @@ resultsContainer.addEventListener("click", async (e) => {
 
   // Fetch latest price from Upstox Sandbox API
   const accessToken =
-    "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI4RkE4R0giLCJqdGkiOiI2OTNlYzJiNTU1MmY0YTYwMDQzNWM0NmUiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6ZmFsc2UsImlhdCI6MTc2NTcyMDc1NywiaXNzIjoidWRhcGktZ2F0ZXdheS1zZXJ2aWNlIiwiZXhwIjoxNzY1NzQ5NjAwfQ.txOBJPc9lG51-km4EHWaVOt9dVbutKtohjjj9XH589I";
+    "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI4RkE4R0giLCJqdGkiOiI2OTNmNjU4YzU1MmY0YTYwMDQzNWM4NDgiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6ZmFsc2UsImlhdCI6MTc2NTc2MjQ0NCwiaXNzIjoidWRhcGktZ2F0ZXdheS1zZXJ2aWNlIiwiZXhwIjoxNzY1ODM2MDAwfQ.uKSESG1Nil6KBZT6dQidSdEeT2TfZUMQJIVXCFgpCRk";
   const priceData = await fetchLatestPrice(instrumentKey, accessToken);
   const contracts = await fetchOptionContracts(instrumentKey, accessToken);
 
