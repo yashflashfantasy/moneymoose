69737a5216de0bf20de183b7c1ea2da282542e8d
Yash Chanda <yash.chanda@adit.com>
2026-02-05 09:08:53 +0530

    Added Fresh tokens

--- assets/js/refresh-stocks.001.js
+++ assets/js/refresh-stocks.001.js
@@ -1,6 +1,6 @@
 async function handleRefresh(data){
     const accessToken =
-    "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI4RkE4R0giLCJqdGkiOiI2OTgyYmM5NjcxMGI1NzM3MzA4ZjA0ZmMiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6ZmFsc2UsImlhdCI6MTc3MDE3NTYzOCwiaXNzIjoidWRhcGktZ2F0ZXdheS1zZXJ2aWNlIiwiZXhwIjoxNzcwMjQyNDAwfQ.diDKyFpd0sDdJtoZUyHILzYlGvLc6ndroufq69AuHRQ";
+    "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI4RkE4R0giLCJqdGkiOiI2OTg0MGUyYzI5NTgwOTQyZTMwY2NiZjQiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6ZmFsc2UsImlhdCI6MTc3MDI2MjA2MCwiaXNzIjoidWRhcGktZ2F0ZXdheS1zZXJ2aWNlIiwiZXhwIjoxNzcwMzI4ODAwfQ.LlMFDgrybstzRnZkiS5y1AdmHMBUwFV9BdtfbvXyTmw";
     const priceData = await fetchLatestPrice(data.instrumentKey, accessToken);
     // await sendToSlack(JSON.stringify(priceData));
     const detailKeys = Object.keys(priceData.data);
